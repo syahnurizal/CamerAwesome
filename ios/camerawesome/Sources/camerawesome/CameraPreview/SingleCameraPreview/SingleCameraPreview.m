f3aa00f23e55fb832d53619ed9c31dced4f92cf9
Syahnurizal <syahnurizal@gmail.com>
2026-08-18 16:00:11 +0800

    Revert "TEMP: instrument camera init timing (NSLog) - will be reverted"
    This reverts commit 3a18c54dee396fbefd05744d5c3e8dbd082388ca.

--- ios/camerawesome/Sources/camerawesome/CameraPreview/SingleCameraPreview/SingleCameraPreview.m
+++ ios/camerawesome/Sources/camerawesome/CameraPreview/SingleCameraPreview/SingleCameraPreview.m
@@ -9,10 +9,6 @@
 
 @implementation SingleCameraPreview {
   dispatch_queue_t _dispatchQueue;
-  // TEMP INSTRUMENTATION (camera init timing investigation, remove after use):
-  // logs wall-clock time from SingleCameraPreview construction to first
-  // preview frame, once per CameraScreen open.
-  BOOL _timingFirstFrameLogged;
 }
 
 - (instancetype)initWithCameraSensor:(PigeonSensorPosition)sensor
@@ -26,11 +22,7 @@
                           completion:(nonnull void (^)(NSNumber * _Nullable, FlutterError * _Nullable))completion
                        dispatchQueue:(dispatch_queue_t)dispatchQueue {
   self = [super init];
-
-  // TEMP INSTRUMENTATION (remove after use):
-  NSLog(@"CAMERA_TIMING_NATIVE init_start %f", CFAbsoluteTimeGetCurrent());
-  _timingFirstFrameLogged = NO;
-
+  
   _completion = completion;
   _dispatchQueue = dispatchQueue;
   
@@ -765,11 +757,6 @@
 
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
   if (output == _captureVideoOutput) {
-    // TEMP INSTRUMENTATION (remove after use):
-    if (!_timingFirstFrameLogged) {
-      _timingFirstFrameLogged = YES;
-      NSLog(@"CAMERA_TIMING_NATIVE first_frame %f", CFAbsoluteTimeGetCurrent());
-    }
     [self.previewTexture updateBuffer:sampleBuffer];
     if (_onPreviewFrameAvailable) {
       _onPreviewFrameAvailable();
